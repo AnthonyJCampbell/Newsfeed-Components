c75fa136fdff4a044557326492818c1b0267f088
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-01-30 15:00:12 +0100

    Added Ian Lunn's Hover library and made the hamburger pulse on :hover

--- Article/Article.js
+++ Article/Article.js
@@ -1,5 +1,8 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
+
+
+// Using our reference to the domElement, toggle a class to expand or hide the article.
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
@@ -24,16 +27,8 @@ class Article {
   }
 }
 
-// Using our reference to the domElement, toggle a class to expand or hide the article.
-
-/* START HERE: 
-
-- Select all classes named ".article" and assign that value to the articles variable.  
-
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
-
-*/
-
+/*- Select all classes named ".article" and assign that value to the articles variable.  
+- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.*/
 let articles = document.querySelectorAll('.article');
 articles.forEach((article) => {
   return new Article(article);
